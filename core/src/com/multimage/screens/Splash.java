e40c78503a1989e708accdf4cef5df2c884204a3
arzarv <arzarv@ttu.ee>
2020-02-29 23:16:40 +0200

    added tween image

--- core/src/com/multimage/screens/Splash.java
+++ core/src/com/multimage/screens/Splash.java
@@ -1,30 +1,43 @@
 package com.multimage.screens;
 
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenAccessor;
+import aurelienribon.tweenengine.TweenManager;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.multimage.tween.SpriteAccessor;
 
 public class Splash implements Screen {
 
     private Sprite splash;
     private SpriteBatch batch;
+    private TweenManager tweenManager;
 
     @Override
     public void show() {
         batch = new SpriteBatch();
+        tweenManager = new TweenManager();
+        Tween.registerAccessor(Sprite.class, new SpriteAccessor());
 
         Texture splashTexture = new Texture("main.jpg");
         splash = new Sprite(splashTexture);
         splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        Tween.set(splash, SpriteAccessor.ALPHA).target(0).start(tweenManager);
+        Tween.to(splash, SpriteAccessor.ALPHA, 2).target(1).start(tweenManager);
+        Tween.to(splash, SpriteAccessor.ALPHA, 2).target(0).delay(2).start(tweenManager);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        tweenManager.update(delta);
 
         batch.begin();
         splash.draw(batch);
