a55ad165fdb03357c859451f32025b8d20c3bd8c
arzarv <arzarv@ttu.ee>
2020-02-29 20:18:44 +0200

    Added a main menu image

---
+++ core/src/com/multimage/screens/Splash.java
@@ -0,0 +1,59 @@
+package com.multimage.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Splash implements Screen {
+
+    private Sprite splash;
+    private SpriteBatch batch;
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+
+        Texture splashTexture = new Texture("main.jpg");
+        splash = new Sprite(splashTexture);
+        splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        splash.draw(batch);
+        batch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
