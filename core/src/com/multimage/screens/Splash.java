f9d78d9000f306858bd81e3bc8f92430809cc540
arzarv <arzarv@ttu.ee>
2020-03-01 13:42:32 +0000

    Merge branch '35-main-screen-with-settings-exit-button-and-play' into 'develop'
    Resolve "main screen exit button and play"
    
    Closes #35
    
    See merge request arzarv/iti0200-2020-project!4
    
    (cherry picked from commit 9c89435a36b198cf72cd121b9d36210005c350eb)
    
    a55ad165 Added a main menu image
    e40c7850 added tween image
    1aff8c8f added MainMenuClass(not finished)
    8c5c0308 Added a main menu image
    fe48e747 Added a main menu image
    72266e17 Added a main menu exit button
    04f5ecc4 Added a main menu exit button

---
+++ core/src/com/multimage/screens/Splash.java
@@ -0,0 +1,77 @@
+package com.multimage.screens;
+
+import aurelienribon.tweenengine.*;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.multimage.tween.SpriteAccessor;
+
+public class Splash implements Screen {
+
+    private Sprite splash;
+    private SpriteBatch batch;
+    private TweenManager tweenManager;
+
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        tweenManager = new TweenManager();
+        Tween.registerAccessor(Sprite.class, new SpriteAccessor());
+
+        Texture splashTexture = new Texture("main.jpg");
+        splash = new Sprite(splashTexture);
+        splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        Tween.set(splash, SpriteAccessor.ALPHA).target(0).start(tweenManager);
+        Tween.to(splash, SpriteAccessor.ALPHA, 2).target(1).repeatYoyo(1, 0.5f).setCallback(new TweenCallback() {
+            @Override
+            public void onEvent(int i, BaseTween<?> baseTween) {
+                ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenu());
+            }
+        }).start(tweenManager);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        tweenManager.update(delta);
+
+        batch.begin();
+        splash.draw(batch);
+        batch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        splash.getTexture().dispose();
+    }
+}
