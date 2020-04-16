12c918d551816c1383cd32e4b0a40616a35e3051
vlsvan <vlsvan@ttu.ee>
2020-04-16 14:09:39 +0300

    second level + camera

---
+++ core/assets/levels/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tileset2" tilewidth="32" tileheight="32" tilecount="6016" columns="64">
+ <image source="tileset2.png" width="2048" height="3008"/>
+</tileset>
