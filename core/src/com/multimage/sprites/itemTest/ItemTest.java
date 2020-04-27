3be8074e74866b7328e3e673b3b116c9afc566be
arzarv <arzarv@ttu.ee>
2020-04-27 04:13:29 +0300

    Fixed TEST dependency on Android SDK

---
+++ core/src/com/multimage/sprites/itemTest/ItemTest.java
@@ -0,0 +1,22 @@
+package com.multimage.sprites.itemTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBonusesFromItems() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItems() {
+    }
+}
